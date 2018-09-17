2153db0f301e6e1f0b4ef0d63f4c5b0011d43d08
Jayesh Badwaik <public@jayeshbadwaik.in>
2018-09-17 14:31:47 +0200

    + initial commit

---
+++ src/include/test/test.hpp
@@ -0,0 +1,8 @@
+#ifndef TEST_TEST_HPP
+#define TEST_TEST_HPP
+
+namespace test {
+char const* version() { return "0.0"; }
+}
+
+#endif // TEST_TEST_HPP
---
+++ test/compile.t.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <test/test.hpp>
+
+int main()
+{
+  std::cout << "Hello, World" << std::endl;
+  return EXIT_SUCCESS;
+}
